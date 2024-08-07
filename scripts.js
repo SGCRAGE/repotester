4a9e4f223bc5991e754801de4af9a09682242250
SGCRAGE <chanceblommaert@gmail.com>
2024-08-07 18:12:31 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+// JavaScript for general site functionality
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page loaded and ready');
+    // Add JavaScript functionality here
+});
 
