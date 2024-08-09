0f8418b9f1f555a7fae2e9dfed15b79990854e91
SGCRAGE <chanceblommaert@gmail.com>
2024-08-09 06:52:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const app = express();
 const port = 3000;
 
 app.use('/api', (req, res) => {
-  const apiUrl = 'http://api.crazyninjaodds.com/api/devigger/v1/sportsbook_devigger.aspx?api=open';
+  const apiUrl = 'http://api.crazyninjaodds.com/api/devigger/v1/sportsbook_devigger.aspx?api=open&Args=ev_p,fb_p,fo_o,kelly,dm';
   request(apiUrl).pipe(res);
 });
 
 app.listen(port, () => {
   console.log(`Proxy server listening at http://localhost:${port}`);
 });
-
