7eac15f33a00be6d89ce9375e76bb672c982be61
SGCRAGE <chanceblommaert@gmail.com>
2024-08-10 06:18:10 -0500

    yueah

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Serve static files from the "public" directory
 app.use(express.static('public'));
+
+// Firebase configuration endpoint
+app.get('/firebase-config', (req, res) => {
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
