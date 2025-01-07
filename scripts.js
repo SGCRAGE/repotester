49c74b18f3ff6b1c8fcbcb593ee2846f0252161c
SGCRAGE <chanceblommaert@gmail.com>
2025-01-06 20:03:38 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const oddsContainer = document.getElementById('odds-container');
     const oddsApiUrl = 'https://api.the-odds-api.com/v4/sports/basketball_nba/odds';
-    const apiKey = '43900254fc7c455464307807da745fd7'; // Your API key
+    const apiKey = 'a3944d813da67c5b4b07199ecdd4affa'; // Your API key
     const regions = 'us'; // Regions
     const markets = 'h2h,spreads'; // Markets
     const oddsFormat = 'american'; // Odds format
